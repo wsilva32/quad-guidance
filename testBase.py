2326b273f437afbc4fee2d1e170e4fee0f2d4812
Steve McGuire <stephen.mcguire@colorado.edu>
2014-12-10 13:31:26 -0700

    Functional base object with test

--- testBase.py
+++ testBase.py
@@ -1,15 +1,24 @@
 import base
 import curses
+import time
+import signal
+import sys
+
+
+useCurses = 1
+drone = None
 
 #Initalization of curses
-screen = curses.initscr()
-screen.clear()
-screen.refresh()
+if useCurses:
+    screen = curses.initscr()
 
-drone = None
 def signal_handler(signal, frame):
     global drone
     drone.stop()
+    if useCurses:
+        screen.clear()
+        screen.refresh()
+        curses.endwin()
     sys.exit(0)
 
 def curses_print(string, line, col):
@@ -36,10 +45,31 @@ def curses_print(string, line, col):
 
 drone = base.DroneBase(vicon_name='wolverine')
 drone.start()
+
+signal.signal(signal.SIGINT, signal_handler)
+
+time.sleep(4)
+if useCurses:
+    screen.clear()
+    screen.refresh()
+
 while (1):
-    curses_print("Roll RC Level: " + str(drone.current_rc_channels[0]), 1, 1)
-    curses_print("Pitch RC Level: " + str(drone.current_rc_channels[1]), 2, 0)
-    curses_print("Throttle RC Level: " + str(drone.current_rc_channels[2]), 3, 0)
-    curses_print("Yaw RC Level: " + str(drone.current_rc_channels[3]), 4, 0)
-    curses_print("Manual Control RC Level: " + str(drone.current_rc_channels[4]), 5, 0)
-    time.sleep(0.5)
+    curLine = 0
+    if useCurses:
+        curses_print("Drone Location: %1.3f, %1.3f, %1.3f" % (drone.get_position()[0], drone.get_position()[1], drone.get_position()[2]), curLine, 0)
+        curLine += 1 
+        curses_print("Balloon Area: %d Centroid: (%d, %d)" % (drone.get_area(), drone.get_centroid()[0], drone.get_centroid()[1]), curLine, 0)
+        curLine += 1 
+        curses_print("Roll RC Level: " + str(drone.current_rc_channels[0]) + '   ',curLine, 0)
+        curLine += 1 
+        curses_print("Pitch RC Level: " + str(drone.current_rc_channels[1]) + '   ', curLine, 0)
+        curLine += 1 
+        curses_print("Throttle RC Level: " + str(drone.current_rc_channels[2]) + '   ', curLine, 0)
+        curLine += 1 
+        curses_print("Yaw RC Level: " + str(drone.current_rc_channels[3]) + '   ', curLine, 0)
+        curLine += 1 
+        curses_print("Manual Control5 RC Level: " + str(drone.current_rc_channels[4]) + '   ',curLine, 0)
+        curLine += 1 
+        curses_print("Manual Control6 RC Level: " + str(drone.current_rc_channels[5]) + '   ', curLine, 0)
+    
+    time.sleep(0.1)
